00e0ce6b64cda3c5abf2d4a89fc9a081b21443e8
Erfder <caherman@fullsail.edu>
2015-01-10 18:44:07 -0700

    duel week 1 publish 1
    getting sorted

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,7 @@
+/**
+  Created by Chase Herman on 1/10/15, week one javascript duel
+ */
+var p1="Empoleon", p2="Infernape"; /*establishes player names in variables*/
+var p1hp=100, p2hp=100;             /*establishes player health*/
+var p1d=20, p2d=20;                 /*creates the max damage var, which seems unnecessary*/
+
