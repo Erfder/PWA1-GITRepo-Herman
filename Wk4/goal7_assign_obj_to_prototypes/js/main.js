b45e673d12d9ec854ab54c4358e17286590963b9
Erfder <caherman@fullsail.edu>
2015-01-29 14:00:39 -0700

    Now Im done
    tested in chrome

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -13,31 +13,37 @@
     //create three instances of Person using a for loop.
     for(var i=0;i<3;i++) {
         //names are being randomly sent to the constructor
-        var personName=Math.floor(Math.random()*names.length);
-        var personFromLoop=new Personfunction(names[personName],i+1); //created after the push method so it has something to push. Calls function in other file using the random name.
+        var randName=Math.floor(Math.random()*names.length);
+        var personFromLoop=new Person(names[randName],i+1); //created after the push method so it has something to push. Calls function in other file using the random name.
 
         //also assigns a row number in the html table
-        populateHTML(personFromLoop.name);
-        populateHTML(personFromLoop.job);
+        populateHTML(personFromLoop.name,"r"+(i+1)+"c1");   //i+1 because the array starts at 0 and tables don't
+        populateHTML(personFromLoop.job,"r"+(i+1)+"c2");    //c# is col, 1 for name 2 for job.
 
         //Every reference gets put in a People array, created above.
         people.push(personFromLoop);
+        names.splice(randName,1); //prevents duplicate names
     }
 
 
     //create a populateHTML function. This outputs name and job.
-    function populateHTML(){
-
+    function populateHTML(displayName,correspondingRow){
+        document.getElementById(correspondingRow).innerHTML=displayName
     }
 
 
-    //An Interval that calls the function below.
+    //An Interval that repeats using rewrite()
+    var interval;
+    clearInterval(interval);
+    interval = setInterval(rewrite, 1000 / 30);
 
     //this function ^^^^.
     function rewrite() {
         //loop through the people array
         people.forEach(function(element){
+
             //run the function on the other file for each
+            element.newAct();
         })
     }
 
--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -5,15 +5,15 @@
 */
 //create a self-executing function.
 (function() {
-    //create a global window object that contains the person object
-    window.Person = Person;
+    //create a global window object that contains the person object set by the function
+    window.Person = Personfunct;
 
     //the person object has 2 static variables. Jobs contains 4+ jobs.
     Person.jobs =["Bowler","Janitor","Physician","Shepherd"];
-    Person.actions=["Conquering Europe","Playing Chess","Driving a station wagon"];
+    Person.actions=["Conquering Europe","Playing Chess","Driving a station wagon","Kicking a can","Spying on neighbors","Cataloguing plants","Making maple syrup"];
 
     //the constructor function is made in this file.
-    function Personfunction(chosenname,chosenrow) {
+    function Personfunct(chosenname,chosenrow) {
         // It has 4 properties: name, action, job, and row. Randomly picks an action and job from above array
         this.name= chosenname;
         this.action= Person.actions[Math.floor(Math.random()*Person.actions.length)]; //rand num between 0 and the max length of the array
@@ -27,8 +27,12 @@
 
 
     //Prototyping: An update function goes here.
-    Person.prototype.newact = function() {
+    Person.prototype.newAct = function() {
         // It changes the action every few seconds, and is called in the other file.
+        if(Math.random()<.008){  //generates a rand num. There is a small chance of the following being executed every time the this function is called. I set this to be lower because my actions take a bit longer to read.
+            this.action=Person.actions[Math.floor(Math.random()*Person.actions.length)]; //selects a random action
+            document.getElementById("r"+this.row+"c3").innerHTML=this.action;
+        }
     }
 
 })(); //end self executing function.
