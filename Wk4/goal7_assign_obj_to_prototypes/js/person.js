9a27df9ccd82c88929fe84b270eb9e1257fb2a2c
Erfder <caherman@fullsail.edu>
2015-01-29 13:38:20 -0700

    just about done

--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -13,12 +13,12 @@
     Person.actions=["Conquering Europe","Playing Chess","Driving a station wagon"];
 
     //the constructor function is made in this file.
-    function Person() {
+    function Personfunction(chosenname,chosenrow) {
         // It has 4 properties: name, action, job, and row. Randomly picks an action and job from above array
-        this.name= ;
+        this.name= chosenname;
         this.action= Person.actions[Math.floor(Math.random()*Person.actions.length)]; //rand num between 0 and the max length of the array
         this.job= Person.jobs[Math.floor(Math.random()*Person.jobs.length)];    //same but with jobs array length
-        this.row= ;
+        this.row= chosenrow;
 
         //using that row, it stores the information so that the HTML can use it.
         document.getElementById("r"+this.row+"c3").innerHTML=this.action; //(goes to doc, gets the id for each row where if this is the third row, it says "r3c3" since all cols will be 3, and writes the action.)
@@ -27,7 +27,7 @@
 
 
     //Prototyping: An update function goes here.
-    Person.prototype.rewrite = function() {
+    Person.prototype.newact = function() {
         // It changes the action every few seconds, and is called in the other file.
     }
 
