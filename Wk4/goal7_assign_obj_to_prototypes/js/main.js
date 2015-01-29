b62ad33e6f79d5bb38768573a4145212a1b257f5
Erfder <caherman@fullsail.edu>
2015-01-29 13:27:38 -0700

    did some more work on main.js
    linked some parts of the functions.

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -6,22 +6,39 @@
 //create a self executing function
 (function() {
     //an array called names that contains at least 5 people
-    var names = ['Mitch', 'Jerome', 'Ashley', 'Preston', 'Rob'];
+    var names = ['Mitch', 'Jerome', 'Ashley', 'Preston', 'Rob'],
+    people=[]
+    ;
 
-    //create three instances of Person using a for loop. Every reference gets put in a People array
+    //create three instances of Person using a for loop.
+    for(var i=0;i<3;i++) {
+        //names are being randomly sent to the constructor
+        var personName=Math.floor(Math.random()*names.length);
+        var personFromLoop=new Personfunction(names[personName],i+1); //created after the push method so it has something to push. Calls function in other file using the random name.
 
-    //names are being randomly sent to the constructor
+        //also assigns a row number in the html table
+        populateHTML(personFromLoop.name);
+        populateHTML(personFromLoop.job);
 
-    //also assigns a row number in the html table
+        //Every reference gets put in a People array, created above.
+        people.push(personFromLoop);
+    }
+
 
     //create a populateHTML function. This outputs name and job.
+    function populateHTML(){
+
+    }
+
 
     //An Interval that calls the function below.
 
     //this function ^^^^.
-
-    //loop through the people array
-
-    //run the function on the other file for each
+    function rewrite() {
+        //loop through the people array
+        people.forEach(function(element){
+            //run the function on the other file for each
+        })
+    }
 
 })(); //end function.
