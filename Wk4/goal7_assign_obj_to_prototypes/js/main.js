28d920a665df087c55b09163ea8de04b21bd4ed4
Erfder <caherman@fullsail.edu>
2015-01-29 12:49:32 -0700

    Filled out the first few pseudocode
    with actual code

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -4,15 +4,15 @@
  * Goal: Goal7
 */
 //create a self executing function
-
+(function() {
     //an array called names that contains at least 5 people
-
+    var names = ['Mitch', 'Jerome', 'Ashley', 'Preston', 'Rob'];
 
     //create three instances of Person using a for loop. Every reference gets put in a People array
 
-        //names are being randomly sent to the constructor
+    //names are being randomly sent to the constructor
 
-        //also assigns a row number in the html table
+    //also assigns a row number in the html table
 
     //create a populateHTML function. This outputs name and job.
 
@@ -20,8 +20,8 @@
 
     //this function ^^^^.
 
-        //loop through the people array
+    //loop through the people array
 
-        //run the function on the other file for each
+    //run the function on the other file for each
 
-//end function.
+})(); //end function.
--- Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -4,21 +4,26 @@
  * Goal: Goal7
 */
 //create a self-executing function.
-
+(function() {
     //create a global window object that contains the person object
-
+    window.Person = Person;
 
     //the person object has 2 static variables. Jobs contains 4+ jobs.
-
+    Person.jobs =["Bowler","Janitor","Physician","Shepherd"];
+    Person.actions=["Conquering Europe","Playing Chess","Driving a station wagon"];
 
     //the constructor function is made in this file.
-
+    function Person() {
         // It has 4 properties: name, action, job, and row.
+        this.name= ;
+        this.action= ;
+        this.job= ;
+        this.row= ;
 
         //using that row, it stores the information so that the HTML can use it.
-
+    }
     //Prototyping: An update function goes here.
 
-        // It changes the action every few seconds, and is called in the other file.
+    // It changes the action every few seconds, and is called in the other file.
 
-//end self executing function.
+})(); //end self executing function.
