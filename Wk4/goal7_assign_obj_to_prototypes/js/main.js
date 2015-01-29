5d37e4fec1ea2e89fb3136fe545557db6bb65fa6
Erfder <caherman@fullsail.edu>
2015-01-29 12:42:31 -0700

    Added pseudocode
    for structure

---
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -0,0 +1,27 @@
+/**
+ * Chase Herman
+ * Class: PWA
+ * Goal: Goal7
+*/
+//create a self executing function
+
+    //an array called names that contains at least 5 people
+
+
+    //create three instances of Person using a for loop. Every reference gets put in a People array
+
+        //names are being randomly sent to the constructor
+
+        //also assigns a row number in the html table
+
+    //create a populateHTML function. This outputs name and job.
+
+    //An Interval that calls the function below.
+
+    //this function ^^^^.
+
+        //loop through the people array
+
+        //run the function on the other file for each
+
+//end function.
---
+++ Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -0,0 +1,24 @@
+/**
+ * Chase Herman
+ * Class: PWA
+ * Goal: Goal7
+*/
+//create a self-executing function.
+
+    //create a global window object that contains the person object
+
+
+    //the person object has 2 static variables. Jobs contains 4+ jobs.
+
+
+    //the constructor function is made in this file.
+
+        // It has 4 properties: name, action, job, and row.
+
+        //using that row, it stores the information so that the HTML can use it.
+
+    //Prototyping: An update function goes here.
+
+        // It changes the action every few seconds, and is called in the other file.
+
+//end self executing function.
